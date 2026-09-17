cc71824bfa49c51794370aff31027b8d8e38d484
Md. Rakibul Hassan Atik <rakibulhassanatik69@gmail.com>
2026-09-17 10:40:04 +0600

    Update practice

--- start.py
+++ start.py
@@ -1,3 +1,30 @@
 #This is bangladesh
 """Go for your work"""
 print("""This is atik from Bangladesh.I want to be an ML Engineer. I am trying to learn ML. I wish I will be able to do those.""")
+my_variable=12 #snake case style
+myVariable=13 #camel case
+MyVariable=14 #pascal case
+print(my_variable," ",myVariable," ",MyVariable)
+my_text="Good morning Atik!"
+print(my_text[:]) #all characters print
+print(my_text[0:6]) #print from G upto m
+print(my_text[5:12])#print whole morning
+print(my_text[5: ])#print from m to last
+print(my_text[::-1])#print from reverse
+print(my_text[-1:-14:-1].upper())#print from ! upto m
+print(my_text[3].capitalize())
+text="hello"
+text="H"+text[1:]
+print(text)
+name="nabid"
+age=45
+print("Nmae :",name, "||","Age: ",age)
+final=name+" "+ "age"+" "+str(age)
+print(final)
+print(f"The name is {name+" hassan"} and the age is {age}")
+print(f"Next year he will be {age+1}")
+pi=3.1416
+print(f"The value upto two decimal is {pi:.2f}")
+print(r"It's a great day \n")#raw string ignores escape sequences,,jaha dibo tahai ashibe!
+print("It'\s a great day \\n")
+
